be2cbf14d616aea98f388d93f49c48eb2c08de5f
quagthanh <dinhquangthanh11@gmail.com>
2026-05-27 13:37:48 +0700

    chore(.github/workflows-axiox_config):add more env into deploy.yml and config for nextjs to choose baseurl

--- HazHzel_App/src/utils/axios-server.ts
+++ HazHzel_App/src/utils/axios-server.ts
@@ -1,8 +1,14 @@
 import { auth } from "@/auth";
 import axios from "axios";
 
+const isServer = typeof window === "undefined";
+
+const baseURL = isServer
+  ? process.env.INTERNAL_BACKEND_URL
+  : process.env.NEXT_PUBLIC_BACKEND_URL;
+
 const http = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
